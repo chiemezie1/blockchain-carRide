58e6fafc809e262de45a3ddd626707bb7acbd1fb
Absolute <106808580+chiemezie1@users.noreply.github.com>
2023-06-06 14:56:24 +0100

    update to about us

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,7 +9,11 @@ function About() {
       <Navbar />
       <div className="py-16">
         <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-          <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+        <h2 className="text-4xl text-center md:text-6xl font-bold font-sans p-8 text-gray-900">
+                Welcome To Block<span className="text-red-900">Ride</span>
+              </h2>
+              <div className="lg:flex">
+          <div className="space-y-6 md:space-y-0 lg:flex md:gap-6 lg:items-start ">
             <div className="rounded-lg md:w-1/2 lg:w-5/12">
               <img
                 src={CarImage}
@@ -18,10 +22,7 @@ function About() {
               />
             </div>
             <div className="md:w-1/2 ">
-              <h2 className="text-2xl md:text-4xl font-bold text-gray-900">
-                Welcome To Block<span className="text-red-900">Ride</span>
-              </h2>
-              <div className="p-4 flex flex-wrap">
+              <div className="flex flex-wrap">
                 <p className="text-lg md:text-2xl font-extrabold leading-normal text-gray-900 my-8">
                   We have a team of transportation experts and blockchain
                   enthusiasts who came together to revolutionize the way people
@@ -46,6 +47,7 @@ function About() {
                   Thank you for choosing BlockRide. We look forward to serving
                   you and being a part of your journey.
                 </p>
+              </div>
               </div>
             </div>
           </div>
